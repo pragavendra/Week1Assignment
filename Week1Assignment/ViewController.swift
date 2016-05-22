791c8edf9a324c8450f752a8f5439f6b9848172f
pragavendra <pragave@l-sb8m11kg8w-m.local>
2016-05-21 21:36:00 -0700

    Initial Commit

---
+++ Week1Assignment/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Week1Assignment
+//
+//  Created by Padmaja Ragavendra on 5/21/16.
+//  Copyright © 2016 Padmaja Ragavendra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
